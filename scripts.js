e70dccef70ec9bf152483df66368e18c447cb34a
Anshuman Tripathi <tripathianshuman18@gmail.com>
2024-11-15 22:27:54 +0530

    Update scripts.js
    when we add any product from index page then there image shown in cart page

--- scripts.js
+++ scripts.js
@@ -1,13 +1,33 @@
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
-function addToCart(productName, productPrice) {
-    // Add product to the cart
-    cart.push({ name: productName, price: productPrice });
-    localStorage.setItem('cart', JSON.stringify(cart));
+// Slideshow functionality
+let slideIndex = 0;
+showSlides();
 
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex - 1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+
+
+    // Add to Cart Functionality
+    function addToCart(productName, productPrice, productImage) {
+        let cart = JSON.parse(localStorage.getItem('cart')) || [];
+        cart.push({ name: productName, price: productPrice, image: productImage });
+        localStorage.setItem('cart', JSON.stringify(cart));
+        document.getElementById('cart-count').innerText = `(${cart.length})`;
+        alert(`${productName} has been added to your cart.`);
+    
     // Update cart count
     document.getElementById('cart-count').innerText = `(${cart.length})`;
-    alert(`${productName} has been added to your cart.`);
+    alert(`${productName} has been added to your cart.`);  
 }
 
 
